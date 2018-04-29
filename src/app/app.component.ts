b32b4f6c63308e5b28b20140ddd1a257ec523cf7
juliano silva <llara.juliano@gmail.com>
2018-04-29 16:52:15 -0300

    Melhoria menu lateral

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,18 +7,24 @@ import { HomePage } from '../pages/home/home';
 import { HomeAlunoPage } from '../pages/home-aluno/home-aluno';
 import { LoginPage } from '../pages/login/login';
 import { VincularAlunosPage } from '../pages/vincular-alunos/vincular-alunos';
+import { AuthServiceProvider } from '../providers/auth-service/auth-service';
+
 @Component({
   selector: 'myapp',
   templateUrl: 'app.html'
 })
 export class MyApp {
+  
   rootPage:any = LoginPage;
 
   public paginas = [
     {titulo: 'Agendamentos', pagina: VincularAlunosPage.name, icone: 'calendar'}
   ];
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, 
+              statusBar: StatusBar, 
+              splashScreen: SplashScreen, 
+              private authService: AuthServiceProvider) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
@@ -26,6 +32,10 @@ export class MyApp {
       statusBar.styleDefault();
       splashScreen.hide();
     });
+  }
+
+  get usuarioLogado () {
+    return this.authService.obtemUsuarioLogado();
   }
 }
 
